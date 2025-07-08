8fd2ce1fc8b14596d554bf732318919207587582
Dmitriy <swagkarpavg@gmail.com>
2025-07-08 03:11:37 +0300

    first committt

--- main.go
+++ main.go
@@ -1,18 +1,4 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println(IsPalindrome("rcar"))
-}
-
-func IsPalindrome(str string) bool {
-	runes := []rune(str)
-	n := len(runes)
-	for i := 0; i < n/2; i++ {
-		if runes[i] != runes[len(runes)-1-i] {
-			return false
-		}
-	}
-	return true
 }
